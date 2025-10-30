e0a6ba6bff0ca32003a755784d92d4c562e4f555
Tien_nu <nguyenthithuytiensmstan@gmail.com>
2025-10-30 23:10:44 +0700

    add slot13

---
+++ slot13/ex2/src/components/ConfirmModal.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+function ConfirmModal({ 
+  show,          // boolean: có hiển thị modal không
+  title,         // tiêu đề modal
+  body, 
+  message,         // nội dung hiển thị trong body
+  confirmText,   // nút xác nhận
+  cancelText,    // nút hủy
+  onConfirm,     // hàm khi nhấn xác nhận
+  onClose        // hàm khi đóng modal
+}) {
+  return (
+    <Modal show={show} onHide={onClose} centered>
+      {title && (
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+      )}
+
+      <Modal.Body>{body}
+        {message}
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onClose}>
+          {cancelText}
+        </Button>
+        {onConfirm && (
+          <Button variant="primary" onClick={onConfirm}>
+            {confirmText}
+          </Button>
+        )}
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default ConfirmModal;
