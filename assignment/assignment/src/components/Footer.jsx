8fd5ed17545ad43ebbdf46989c47c95e2e49fa6a
Tien_nu <nguyenthithuytiensmstan@gmail.com>
2025-11-14 08:33:38 +0700

    add assignment

---
+++ assignment/assignment/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-center bg-dark py-3">
+      <small style={{color: "white"}}>© 2025 PersonalBudget</small>
+    </footer>
+  );
+}
